de747cf85b8fa9c8211b946e4d767e65dd453fb0
guamanEd <edwinsg96@gmail.com>
2021-05-13 20:13:44 -0400

    handle click + render to screen

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
-function searchImg () {
-    fetch ("http://api.giphy.com/v1/gifs/search?api_key=ep8j72PvIfJJLKq5p8x7pvmvoxyqMcby&q=#&limit=5")
+let APIKEY = "ep8j72PvIfJJLKq5p8x7pvmvoxyqMcby";
+
+function searchImg (emotion) {
+    fetch ("http://api.giphy.com/v1/gifs/search?api_key=ep8j72PvIfJJLKq5p8x7pvmvoxyqMcby&q=" + emotion + "&limit=1")
         .then(function(response){
             return response.json()
         })
         .then(function(data){
-        console.log(data.data)    
+            renderToScreen (data.data[0])
+        // console.log(data.data[0].images.original.url)    
          }
         )
         };
 
+function init() {
+    document.getElementById("Emojis").addEventListener("click", handleClick)
+}
+function handleClick (ev) {
+    var mood = ev.target.dataset.mood;
+    if (mood) {
+        searchImg(mood)
+    }
+}
 
-let APIKEY = "ep8j72PvIfJJLKq5p8x7pvmvoxyqMcby";
+function renderToScreen (memeData) {
+    console.log(memeData)
+} 
 
 document.addEventListener("DOMContentLoaded", init);
-function init() {
-  document.getElementById("Emojis").addEventListener("click", ev => {
-    ev.preventDefault();
-    let url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=1&q=`;
-
-})
-
- 
+// let url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=1&q=`;
